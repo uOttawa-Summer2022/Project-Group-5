0a5172a6500dfac165d893c541c35d2276aa4d90
Matthew Tang <mtang046@uottawa.ca>
2022-06-05 23:26:35 -0400

    initial prototype pages

---
+++ app/src/main/java/com/example/course_booking/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.course_booking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private Button login;
+    private TextView signup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        login = (Button) findViewById(R.id.btnLogin);
+        signup = (TextView) findViewById(R.id.signUp);
+
+
+        //When Login button is pressed
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        //When Signup Button is pressed
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent intent = new Intent(this, SignupActivity);
+                //startActivity(intent);
+            }
+        });
+    }
+
+}
