e569eb3b60a32b94e788000d431cb1bb6fa7d0d0
Weiwei Zhang <wzhan228@uottawa.ca>
2022-06-08 11:45:16 -0400

    log in and signup

--- app/src/main/java/com/example/course_booking/MainActivity.java
+++ app/src/main/java/com/example/course_booking/MainActivity.java
@@ -1,16 +1,29 @@
 package com.example.course_booking;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+    Activity context;
+    EditText username,password;
     private Button login;
     private TextView signup;
+    String email;
+    TextView tv2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +32,28 @@ public class MainActivity extends AppCompatActivity {
 
         login = (Button) findViewById(R.id.btnLogin);
         signup = (TextView) findViewById(R.id.signUp);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        tv2 = findViewById(R.id.textView2);
 
 
         //When Login button is pressed
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(username.getText().toString(),password.getText().toString()).addOnCompleteListener(context, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                            tv2.setText("Result: Login successful");
+                            email = user.getEmail();
 
+                        }else{
+                            tv2.setText("Result : login failed"+task.getException());
+                        }
+                    }
+                });
             }
         });
 
@@ -33,8 +61,11 @@ public class MainActivity extends AppCompatActivity {
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Intent intent = new Intent(this, SignupActivity);
-                //startActivity(intent);
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this,SignupActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                intent.putExtra("email",email);
+                startActivity(intent);
             }
         });
     }
--- app/src/main/java/com/example/course_booking/SignupActivity.java
+++ app/src/main/java/com/example/course_booking/SignupActivity.java
@@ -1,14 +1,60 @@
 package com.example.course_booking;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SignupActivity extends AppCompatActivity {
+    Activity context = this;
+    EditText username,password;
+    Button btnSignUp;
+    TextView tv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
+        
+        username = findViewById(R.id.usernameForm);
+        password = findViewById(R.id.passwordForm);
+        btnSignUp = findViewById(R.id.btnSignup);
+        tv = findViewById(R.id.textView);
+        
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signup();
+            }
+        });
+        
+    }
+
+    private void signup() {
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(username.getText().toString(),password.getText().toString()).addOnCompleteListener(context,new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    tv.setText("Result: "+user.getEmail()+"Registration success and you can login in");
+
+                } else {
+                    tv.setText("Result: Registration failed"+task.getException());
+                }
+            }
+        });
     }
 }
