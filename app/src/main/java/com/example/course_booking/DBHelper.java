15c91dbfa68ff9d92c9e61b62ae57e44254d3223
Yannick V <90577014+bravesirrobin358@users.noreply.github.com>
2022-06-12 19:52:44 -0400

    insertData:added accType support

--- app/src/main/java/com/example/course_booking/DBHelper.java
+++ app/src/main/java/com/example/course_booking/DBHelper.java
@@ -9,13 +9,17 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DBHelper extends SQLiteOpenHelper {
 
     public static final String DBNAME = "Login.db";
+    private static final String TABLE_NAME = "users";
+    private static final String COLUMN_PASSWORD = "password";
+    private static final String COLUMN_USERNAME = "name";
+    private static final String COLUMN_TYPE = "accType";
 
     public DBHelper(Context context){
         super(context, "Login.db",null,1);
     }
     @Override
     public void onCreate(SQLiteDatabase MyDB) {
-        MyDB.execSQL("create Table users(name TEXT primary key, password TEXT, accType TEXT)");
+        MyDB.execSQL("create Table TABLE_NAME(COLUMN_USERNAME TEXT primary key, COLUMN_PASSWORD TEXT, COLUMN_TYPE TEXT)");
 
     }
 
@@ -25,12 +29,13 @@ public class DBHelper extends SQLiteOpenHelper {
 
     }
 
-    public boolean insertData(String name, String password){
+    public boolean insertData(String name, String password, Type accType){
         SQLiteDatabase myDB = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put("name", name);
         contentValues.put("password", password);
-        long result = myDB.insert("users",null,contentValues);
+        contentValues.put("accType", String.valueOf(accType));
+        long result = myDB.insert(TABLE_NAME,null,contentValues);
         if (result == -1){
             return false;
         }else {
