917df0b22356358685d790ae92ab836b4a9f4082
Yannick V <90577014+bravesirrobin358@users.noreply.github.com>
2022-06-12 19:51:33 -0400

    onClick: added accType as parameter

--- app/src/main/java/com/example/course_booking/SignupActivity.java
+++ app/src/main/java/com/example/course_booking/SignupActivity.java
@@ -1,27 +1,19 @@
 package com.example.course_booking;
 
 import android.app.Activity;
+import android.database.Cursor;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.sql.DatabaseMetaData;
 
@@ -39,33 +31,45 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
-        
+
         username = findViewById(R.id.usernameForm);
         password = findViewById(R.id.passwordForm);
         btnSignUp = findViewById(R.id.btnSignup);
         returnLogin = findViewById(R.id.returnLogin);
+        RadioGroup radiogroup = (RadioGroup) findViewById(R.id.grpAccType);
+
+
+        // find the radio button by returned id
 
         db = new DBHelper(this);
 
 
-        
+
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                int selectedId = radiogroup.getCheckedRadioButtonId();
+                RadioButton radioBtn = (RadioButton) findViewById(selectedId);
                 //get data from EditTexts into String variables
                 String nameTxt = username.getText().toString();
                 String passwordTxt = password.getText().toString();
+                String accTypeSTR = (String) radioBtn.getText().toString();
+                Type accType = null;
+                if (accTypeSTR.equals("Student")){
+                    accType = Type.STUDENT;
+                } else if (accTypeSTR.equals("Instructor")){
+                    accType = Type.INSTRUCTOR;
+                }
 
                 //check if user fill all the fields before sending data to firebase
                 if(nameTxt.isEmpty() || passwordTxt.isEmpty()){
                     Toast.makeText(SignupActivity.this, "Please fill all fields", Toast.LENGTH_SHORT).show();
-                }
-                else{
-
+                } else if(accType == null){
+                    Toast.makeText(SignupActivity.this, "Select Student or Instructor as account type", Toast.LENGTH_SHORT).show();
+                }else{
                     boolean checkuser = db.checkusername(nameTxt);
                     if(!checkuser){
-                        boolean insert = db.insertData(nameTxt,passwordTxt);
+                        boolean insert = db.insertData(nameTxt,passwordTxt,accType);
                         if(insert){
                             Toast.makeText(SignupActivity.this,"Registered successfully",Toast.LENGTH_SHORT).show();
 
@@ -90,7 +94,7 @@ public class SignupActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), MainActivity.class));
             }
         });
-        
+
     }
 
 
