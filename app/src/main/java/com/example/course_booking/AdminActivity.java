00fe6b88c9c911b7d9b07db0cf525ce73cbc6d78
Weiwei Zhang <wzhan228@uottawa.ca>
2022-06-14 17:23:54 -0400

    add EditText

--- app/src/main/java/com/example/course_booking/AdminActivity.java
+++ app/src/main/java/com/example/course_booking/AdminActivity.java
@@ -6,12 +6,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class AdminActivity extends AppCompatActivity{
     TextView welcomeAdmin;
     DBHelper db;
     Button createCourse,editCourse,deleteCourse,deleteAccount;
+    EditText crsCode,crsName,accName;
 
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -29,6 +31,9 @@ public class AdminActivity extends AppCompatActivity{
         editCourse = findViewById(R.id.btn_editCourse);
         deleteCourse = findViewById(R.id.btn_deleteCourse);
         deleteAccount = findViewById(R.id.btn_deleteAccount);
+        crsCode = findViewById(R.id.crsCodeForm);
+        crsName = findViewById(R.id.crsNameForm);
+        accName = findViewById(R.id.accNameForm);
 
 
         createCourse.setOnClickListener(new View.OnClickListener() {
@@ -56,7 +61,7 @@ public class AdminActivity extends AppCompatActivity{
         deleteAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+
             }
         });
 
