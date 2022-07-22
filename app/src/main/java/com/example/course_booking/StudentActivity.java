4035070b03e25aa984bf8cb742165945a7886903
Matthew Tang <mtang046@uottawa.ca>
2022-07-22 18:32:13 -0400

    setup xml and activity

--- app/src/main/java/com/example/course_booking/StudentActivity.java
+++ app/src/main/java/com/example/course_booking/StudentActivity.java
@@ -6,19 +6,34 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class StudentActivity extends AppCompatActivity{
+    EditText crsCode,crsName, day;
+    ListView crsListView;
     TextView welcomeStudent;
     DBHelper db;
-    Button btnLogout;
+    DBHelper_course db_course;
+    Button btnLogout, btnViewAll, btnSearch, btnEnroll, btnUnenroll;
 
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.student);
 
+        //EditText
+        crsCode = findViewById(R.id.et_crsCode2);
+        crsName = findViewById(R.id.et_crsName2);
+        day = findViewById(R.id.et_day);
+
+        //Buttons
         btnLogout = findViewById(R.id.btnLogoutStudent);
+        btnViewAll = findViewById(R.id.btn_viewAll2);
+        btnSearch = findViewById(R.id.btn_search2);
+        btnEnroll = findViewById(R.id.btn_enroll);
+        btnUnenroll = findViewById(R.id.btn_unenroll);
 
         welcomeStudent = (TextView)findViewById(R.id.welcomeStudent);
         db = new DBHelper(this);
