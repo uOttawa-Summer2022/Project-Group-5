428eb0580d19f84e7935b7c0bad5682a12a9aec7
Matthew Tang <mtang046@uottawa.ca>
2022-07-22 22:41:46 -0400

    Added JUnit Tests

--- app/src/main/java/com/example/course_booking/Validator.java
+++ app/src/main/java/com/example/course_booking/Validator.java
@@ -1,6 +1,9 @@
 package com.example.course_booking;
 
 
+import android.util.Log;
+import android.view.ViewDebug;
+
 public class Validator {
 
     String usernameR = "[A-Za-z]+";
@@ -26,6 +29,36 @@ public class Validator {
         if(endHour < 0 || endHour > 23){return false;}
 
         return endMin >= 0 && endMin <= 59;
+    }
+
+    public boolean validDay(String day) {
+        for (Day d : Day.values()) {
+            if (d.name().equals(day)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean validTime(Session s1, Session s2)  {
+
+        if (!s1.getDay().equals(s2.getDay())) {
+            return true;
+        }
+        else if (s1.getStartHour() < s2.getStartHour() && s1.getEndHour() > s2.getStartHour()) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    public boolean validCapacity(CourseModel cm) {
+        if (cm.getCrsCapacity() > 0) {
+            return true;
+        }
+        return false;
     }
 
 }
--- app/src/test/java/com/example/course_booking/ValidatorTest.java
+++ app/src/test/java/com/example/course_booking/ValidatorTest.java
@@ -53,4 +53,46 @@ public class ValidatorTest {
         int startHour = 1000, startMin = 300, endHour = 10, endMin = 45;
         assertFalse(validator.validSession(startHour,startMin, endHour, endMin));
     }
+
+    @Test
+    public void validDay_true() {
+        assertTrue(validator.validDay("MONDAY"));
+    }
+
+    @Test
+    public void validDay_false() {
+        assertFalse(validator.validDay("SOMEDAY"));
+    }
+
+    @Test
+    public void validTime_true() {
+        Session s1 = new Session(Day.MONDAY, 8, 00, 9, 50);
+        Session s2 = new Session(Day.TUESDAY, 10, 00, 11, 20);
+
+        assertTrue(validator.validTime(s1, s2));
+    }
+
+    @Test
+    public void validTime_false() {
+        Session s1 = new Session(Day.MONDAY, 8, 00, 11, 00);
+        Session s2 = new Session(Day.MONDAY, 9, 30, 11, 50);
+
+        assertFalse(validator.validTime(s1, s2));
+    }
+
+    @Test
+    public void validCapacity_true() {
+        CourseModel cm = new CourseModel();
+        cm.setCrsCapacity(10);
+
+        assertTrue(validator.validCapacity(cm));
+    }
+
+    @Test
+    public void validCapacity_false() {
+        CourseModel cm = new CourseModel();
+        cm.setCrsCapacity(0);
+
+        assertFalse(validator.validCapacity(cm));
+    }
 }
